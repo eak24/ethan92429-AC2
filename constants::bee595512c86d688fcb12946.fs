9f411330f35d51749a8a193ba3f6969f75850ad0
Ethan Keller <ethan.keller@gmail.com>
2019-08-31 13:18:24 -0400

    my commit message

---
+++ constants::bee595512c86d688fcb12946.fs
@@ -0,0 +1,6 @@
+FeatureScript 1135;
+import(path : "onshape/std/geometry.fs", version : "1135.0");
+
+export const VC_ORIFICE_RATIO = 0.63;
+
+export const DRILL_BITS = [0.125*inch, 0.25*inch,0.375*inch,0.5*inch,0.625*inch,0.75*inch,0.875*inch,1*inch,1.25*inch,1.5*inch,1.75*inch,2*inch];
